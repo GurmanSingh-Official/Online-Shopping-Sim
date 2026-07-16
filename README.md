b7dd0ce51b022e752b92ecd3207f607cc6042ca4
GurmanSingh-Official <gurmansingh.official18@gmail.com>
2026-07-16 20:45:52 +0530

    Added Exit and view products Output

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 This is a Terminal based Online Shopping Simulator. I am doing this to practice the concepts of python I have learned till now. 
 
 1. Added User Task Input.
+2. Added Exit and view products Output
+
+Conditionals in line 15 and 28 and like these are to be removed,
+since these are for test and a function for user task output will 
+be made separately to improve efficiency.
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def user_inp():
+import time
+
+def user_inp(): # Takes user input
     print("Hey")
     print("What do you want to do?")
     print("1. View Products")
@@ -7,5 +9,31 @@ def user_inp():
     print("4. Exit")
     inp = input("").strip().lower()
     return inp
+user_task = user_inp()
 
-user_inp()
+def quit(): # program for defining exit criteria
+    if user_task == "exit" or user_task == "4": 
+        print("Exitting....")
+        time.sleep(2)
+        print("Exitted!")
+        exit()
+quit()
+
+products = {"Product": "Price", 
+            "Apple":    45,
+            "Milk":     60,
+            "Mouse":    399}
+# List of products, For Demo right now only 3 products are available.
+def view_p():
+    if user_task == "view products" or user_task == "1":
+        print("Fetching products...")
+        time.sleep(2)
+        print("Here are the available products:-")
+        for keys, values in products.items():
+            print(keys, values)
+        print("Prices are in rupees.")
+view_p()
+
+# Conditionals in line 15 and 28 and like these are to be removed,
+# since these are for test and a function for user task output will
+# be made separately to improve efficiency. 
