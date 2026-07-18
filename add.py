92fd31e5f1fa04b84ce978bbd6e0d32dec8cb530
GurmanSingh-Official <gurmansingh.official18@gmail.com>
2026-07-18 17:51:51 +0530

    Adding add.py file for convinience in order branch

---
+++ add.py
@@ -0,0 +1,13 @@
+import main 
+
+class Items:
+    def __init__(self, prod, qty):
+        prod = prod.lower()
+        self.prod = prod
+        self.qty = qty 
+
+def check():
+    ...
+    
+
+    
--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def user_inp(): # Takes user input
     print("4. Exit")
     inp = input("").strip().lower()
     return inp
-user_task = user_inp()
+
+if __name__ == "__main__":
+    user_task = user_inp()
 
 def quit(): # program for defining exit criteria
     if user_task == "exit" or user_task == "4": 
@@ -17,23 +19,29 @@ def quit(): # program for defining exit criteria
         time.sleep(2)
         print("Exitted!")
         exit()
-quit()
+if __name__ == "__main__":
+    quit()
 
 products = {"Product": "Price", 
-            "Apple":    45,
-            "Milk":     60,
-            "Mouse":    399}
+            "apple":    45,
+            "milk":     60,
+            "mouse":    399}
+
 # List of products, For Demo right now only 3 products are available.
 def view_p():
     if user_task == "view products" or user_task == "1":
         print("Fetching products...")
         time.sleep(2)
         print("Here are the available products:-")
+        print("")
         for keys, values in products.items():
-            print(keys, values)
+            print(keys.capitalize(), values)
         print("Prices are in rupees.")
-view_p()
+        print("")
+if __name__ == "__main__":       
+    view_p()
 
 # Conditionals in line 15 and 28 and like these are to be removed,
 # since these are for test and a function for user task output will
 # be made separately to improve efficiency. 
+
