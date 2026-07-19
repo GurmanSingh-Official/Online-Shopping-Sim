5aca9eb61fc641f1ab06f5a0d67b10690ac857c7
GurmanSingh-Official <gurmansingh.official18@gmail.com>
2026-07-19 22:58:08 +0530

    Adding Input Check Feature

--- add.py
+++ add.py
@@ -2,12 +2,26 @@ import main
 
 class Items:
     def __init__(self, prod, qty):
-        prod = prod.lower()
         self.prod = prod
         self.qty = qty 
 
-def check():
-    ...
+def input_check():
+    product = input("Product Name: ").lower().strip()
+    if product not in main.products.keys():
+        print("We are Sorry, This Product is not available right now.")
+        input_check()
+    else:
+        print("Product is available.")
+        quantity = int(input("Product Quantity: "))
+        prod = Items(product, quantity) 
+        return prod      
+input_check()
+
+
+
+
+
+
     
 
     
