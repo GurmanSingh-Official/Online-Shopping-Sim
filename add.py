235626669a9e9d3c624e1b169cf87d9a25deaf5a
GurmanSingh-Official <gurmansingh.official18@gmail.com>
2026-07-19 23:11:08 +0530

    Debugging

--- add.py
+++ add.py
@@ -4,20 +4,20 @@ class Items:
     def __init__(self, prod, qty):
         self.prod = prod
         self.qty = qty 
+        
 
 def input_check():
     product = input("Product Name: ").lower().strip()
     if product not in main.products.keys():
         print("We are Sorry, This Product is not available right now.")
-        input_check()
+        return input_check()
     else:
-        print("Product is available.")
+        print(f"{product.capitalize()} is available.")
         quantity = int(input("Product Quantity: "))
-        prod = Items(product, quantity) 
-        return prod      
-input_check()
-
-
+        p = Items(product, quantity) 
+        return p      
+items = input_check()
+print(items.prod.capitalize())
 
 
 
